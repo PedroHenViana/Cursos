53e74d6bdade535b2b5687469aa45933e2b0cae3
PedroHenViana <pedroviana6157@gmail.com>
2025-07-04 15:45:01 -0300

    Atualização

--- node.js/Aula 05/index.js
+++ node.js/Aula 05/index.js
@@ -1,10 +1,27 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+const connection = require("./database/database");
 
-// Estou dizendo para o Express usar o EJS como View Engine
+//Database
+connection
+    .authenticate()
+    .then(() => {
+        console.log("Conexão feita com o banco de dados!")
+    })
+    .catch((msgErro) => {
+        console.log(msgErro);
+    })
+
+//Estou dizendo para o Express usar o EJS como View Engine
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
+//Body Parser
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+//Rotas
 app.get("/", (req, res) => {
     res.render("index");
 });
@@ -14,7 +31,9 @@ app.get("/perguntar",(req, res) => {
 });
 
 app.post("/salvarpergunta", (req, res) => {
-    res.send("Formulário recebido!")
+    var titulo = req.body.titulo;
+    var descricao = req.body.descricao;
+    res.send("Formulário recebido!" + titulo + " " + " descricao " + descricao);
 });
 
 app.listen(3306, () => {console.log("App rodando!");});
